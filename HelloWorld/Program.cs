8a301d1e315efdae5bdab6be409d428482779386
Dara Oladapo <daraoladapo@outlook.com>
2022-02-22 09:30:52 +0000

    add another commented code.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,6 +4,30 @@ namespace HelloWorld
     class Program
     {
         public static void Main(string[] args){
+            //Console.WriteLine("Hello, what is your name?: ");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine("How old are you?: (use just numbers)");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine("How much do you have in your bank account?:");
+            //double amountInBank = double.Parse(Console.ReadLine());
+            //Console.WriteLine($"Hello {userName}, you are {age} years old and have £{amountInBank}.");
+
+            //const double piValue = 3.142;
+            //double circleRadius = 5.2;
+            //var areaOfCircle = piValue * circleRadius * circleRadius;
+
+            //double circleRadius = 5.2;
+            //var areaOfCircle = Math.PI * Math.Pow(circleRadius, 2);
+            //Console.WriteLine(areaOfCircle);
+            //Add: +
+            //Substract: -
+            //Divide: /
+            //Multiply: *
+            //Assign: =
+            //var squareRootOf25 = Math.Sqrt(25);
+            //Console.WriteLine(squareRootOf25);
+            //var cubeRootOf125 = Math.Pow(125, (float)1/3);
+            //Console.WriteLine(cubeRootOf125);
             bool eligible;
             int age;
             Console.WriteLine("Enter your age: ");

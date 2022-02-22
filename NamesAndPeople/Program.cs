c4879e72f2b36c3f9fb9a7fff30f625355beef08
Dara Oladapo <daraoladapo@outlook.com>
2022-02-22 15:07:59 +0000

    annotations

--- NamesAndPeople/Program.cs
+++ NamesAndPeople/Program.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args)
         {
+            //create new instance of person
             Person person = new Person();
             person.FirstName = "John";
             person.MiddleName = "Michael";
@@ -12,18 +13,24 @@
             Console.WriteLine($"Name in full: {person.GetFullName()}");
         }   
     }
+    //create class Person
     public class Person{
+        //create fields
         public string FirstName;
         public string MiddleName{ get; set; }
         public string LastName{ get; set; }
+
+        //create readonly field
         public string Initials
         {
             get { return $"{FirstName[0]}.{MiddleName[0]}."; }
         }
+        //get initials
         public string GetNameInInitials()
         {
             return $"{Initials} {LastName}";
-        }    
+        }  
+        //get full name  
         public string GetFullName()
         {
             return $"{FirstName} {MiddleName} {LastName}";
--- ShapesConsoleApp/Program.cs
+++ ShapesConsoleApp/Program.cs
@@ -6,29 +6,50 @@ namespace ShapesConsoleApp
     {
         static void Main(string[] args)
         {
-            Rectangle niceRectangle = new Rectangle(12.5, 52);
-            var rectangleArea = niceRectangle.GetArea();
-            Console.WriteLine(rectangleArea);
-            var rectanglePerimeter = niceRectangle.GetPerimeter();
-            Console.WriteLine(rectanglePerimeter);
+            // Rectangle niceRectangle = new Rectangle(12.5, 52);
+            // Rectangle niceRectangle = new Rectangle();
+            // var rectangleArea = niceRectangle.GetArea(12.5,52);
+            // Console.WriteLine(rectangleArea);
+
+            // Rectangle anotherRectange = new Rectangle();
+            // var anotherRectangleArea = anotherRectange.GetArea(18.5, 5);
+            // var rectanglePerimeter = niceRectangle.GetPerimeter();
+            // Console.WriteLine(rectanglePerimeter);
+            // niceRectangle.Speak();
+            // PropertiesInCSharp propInCS = new PropertiesInCSharp();
+            // propInCS.Greetings();
+            // Console.WriteLine(propInCS.FirstName);
+            // Console.ReadKey();
         }
     }
     class Rectangle
     {
         private double length;
         private double width;
+        public Rectangle()
+        {
+        }
         public Rectangle(double l, double w)
         {
             length = l;
             width = w;
         }
-        public double GetArea()
+        public double GetArea(double length, double width)
         {
+            //this is a commment
+            var area = length * width;
+            return area;
+        }
+        public double GetArea(){
             return length * width;
         }
         public double GetPerimeter()
         {
             return 2 * (length + width);
+        }
+  
+        public void Speak(){
+            Console.WriteLine("I am a rectangle");
         }
     }
 }

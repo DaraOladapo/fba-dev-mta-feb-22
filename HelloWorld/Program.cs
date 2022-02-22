43f1e2ca1d391a6d7946211c38c894151e170ddc
Dara Oladapo <daraoladapo@outlook.com>
2022-02-22 09:23:09 +0000

    init

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args){
+            bool eligible;
+            int age;
+            Console.WriteLine("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            eligible = (age >= 18);
+            while (eligible)
+            {
+                Console.WriteLine("Runs because eligible is true");
+                eligible = false;
+            }
+
+            do
+            {
+                Console.WriteLine("Runs anyways.");
+                eligible = false;
+            } while (eligible);
+
+        for (var i = 5; i <= 100; i+=5)
+        {
+            Console.WriteLine(i);
+        }
+        
+        string[] names = {"John", "Jane", "Joe", "Jack", "Jill", "Jenny", "Jared"};
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+        for (var i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+        }
+        }
+    }
+}

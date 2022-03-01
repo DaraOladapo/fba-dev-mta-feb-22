9ba2c5e33080ed9bfe4d36308115bb19db664cab
Dara Oladapo <daraoladapo@outlook.com>
2022-03-01 10:50:23 +0000

    ASP.NET Web Apps

---
+++ WebApplication3/Controllers/CarController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            var car = new Car() { Make = "Toyota", Model = "RAV4" };
+            return View(car);
+        }
+    }
+}
---
+++ WebApplication3/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
